a5dd3b66d4256ddcc2061d7f3f26c0a3f3cdff72
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2023-08-28 20:39:13 +0300

    feat: add functionallity to Finallize button on ReservationPage feat: add text checking to form of ReservationPage

--- car-rental-app/Views/ReservationPage.xaml.cs
+++ car-rental-app/Views/ReservationPage.xaml.cs
@@ -13,6 +13,9 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using car_rental_app.Data;
+using Windows.System;
+using Microsoft.WindowsAppSDK.Runtime.Packages;
+using MySqlConnector;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -38,9 +41,9 @@ namespace car_rental_app.Views
         {
             this.InitializeComponent();
 
-            if (User.Instance.CardExpirationDate != null)
+            if (Data.User.Instance.CardExpirationDate != null)
             {
-                string[] values = User.Instance.CardExpirationDate.Split("/");
+                string[] values = Data.User.Instance.CardExpirationDate.Split("/");
                 CardExpirationMonth = values[0];
                 CardExpirationYear = values[1];
             }
@@ -73,14 +76,137 @@ namespace car_rental_app.Views
             }
         }
 
-        private void Finallize_Button_Click(object sender, RoutedEventArgs e)
+        private async void Finallize_Button_Click(object sender, RoutedEventArgs e)
         {
+            if (CardNumberNumberBox.Text.Length == 12 && (CardExpirationMonthTextBox.Text.Length == 2 || CardExpirationMonthTextBox.Text.Length == 1) && CardExpirationYearTextBox.Text.Length == 2 && CardSecurityCodeTextBox.Text.Length == 3)
+            {
+                try
+                {
+                    string connectionString = "Server=localhost;Port=3306;Database=car_rental_app;Uid=root;Pwd=;";
+                    using MySqlConnection connection = new MySqlConnection(connectionString);
+                    await connection.OpenAsync();
+
+                    string query = "INSERT INTO Reservation (UserId, CarId, FromDate, ToDate) VALUES (@UserId, @CarId, @FromDate, @ToDate)";
+                    using MySqlCommand command = new MySqlCommand(query, connection);
+
+                    command.Parameters.AddWithValue("@UserId", 2);
+                    command.Parameters.AddWithValue("@CarId", carId);
+                    command.Parameters.AddWithValue("@FromDate", fromDate);
+                    command.Parameters.AddWithValue("@ToDate", toDate);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        string selectQuery = "SELECT Id FROM Reservation WHERE FromDate = @FromDate AND ToDate = @ToDate AND CarId = @CarId";
+                        using MySqlCommand selectCommand = new MySqlCommand(selectQuery, connection);
+
+                        selectCommand.Parameters.AddWithValue("@FromDate", fromDate);
+                        selectCommand.Parameters.AddWithValue("@ToDate", toDate);
+                        selectCommand.Parameters.AddWithValue("@CarId", carId);
+
+                        using MySqlDataReader reader = await selectCommand.ExecuteReaderAsync();
+
+                        while (await reader.ReadAsync())
+                        {
+                            int id = reader.GetInt32("Id");
+                            Reservation.Instance.Add(new Reservation(id, carId, fromDate, toDate, car.Name));
+                        }
+                        Frame.Navigate(typeof(ViewCarsPage));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
 
         }
 
         private void Cancel_Button_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(ViewCarsPage));
+        }
+
+        private void CardNumberNumberBox_BeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            foreach (char c in args.NewText)
+            {
+                if (!char.IsDigit(c))
+                {
+                    args.Cancel = true;
+                    return;
+                }
+            }
+
+            if (args.NewText.Length > 12)
+            {
+                CardExpirationMonthTextBox.Focus(FocusState.Programmatic);
+                args.Cancel = true; // Cancel the text change if it exceeds the maximum length
+                return;
+            }
+        }
+
+        private void CardExpirationMonthTextBox_BeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            foreach (char c in args.NewText)
+            {
+                if (!char.IsDigit(c))
+                {
+                    args.Cancel = true;
+                    return;
+                }
+            }
+
+            if (args.NewText.Length > 2)
+            {
+                CardExpirationYearTextBox.Focus(FocusState.Programmatic);
+                args.Cancel = true; // Cancel the text change if it exceeds the maximum length
+                return;
+            }
+
+            if (int.Parse(args.NewText) < 0 || int.Parse(args.NewText) > 12)
+            {
+                args.Cancel = true;
+                return;
+            }
+        }
+
+        private void CardExpirationYearTextBox_BeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            foreach (char c in args.NewText)
+            {
+                if (!char.IsDigit(c))
+                {
+                    args.Cancel = true;
+                    return;
+                }
+            }
+
+            if (args.NewText.Length > 2)
+            {
+                CardSecurityCodeTextBox.Focus(FocusState.Programmatic);
+                args.Cancel = true; // Cancel the text change if it exceeds the maximum length
+                return;
+            }
+        }
+
+        private void CardSecurityCodeTextBox_BeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            foreach (char c in args.NewText)
+            {
+                if (!char.IsDigit(c))
+                {
+                    args.Cancel = true;
+                    return;
+                }
+            }
+
+            if (args.NewText.Length > 3)
+            {
+                args.Cancel = true; // Cancel the text change if it exceeds the maximum length
+                return;
+            }
         }
     }
 
