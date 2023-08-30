1dfede8f0843b78bddfe5b4af81baff9659afee2
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2023-08-31 00:40:36 +0300

    feat: add functionality to AddCarPage

--- car-rental-app/Views/AddCarPage.xaml.cs
+++ car-rental-app/Views/AddCarPage.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using MySqlConnector;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +28,9 @@ namespace car_rental_app.Views
         List<string> sizes = new List<string>();
         List<string> transmissionTypes = new List<string>();
         List<string> fuelTypes = new List<string>();
+
+        Boolean isLoading = false;
+
         public AddCarPage()
         {
             sizes.Add("Compact");
@@ -42,6 +47,71 @@ namespace car_rental_app.Views
             fuelTypes.Add("Electric");
 
             this.InitializeComponent();
+        }
+
+        private async Task<Boolean> AddCar()
+        {
+            string name = NameTextBox.Text;
+            string size = SizeComboBox.SelectedItem.ToString();
+            string transmissionType = TransmissionTypeComboBox.SelectedItem.ToString();
+            string fuelType = FuelTypeComboBox.SelectedItem.ToString();
+            double pricePerDay = double.Parse(PriceTextBox.Text);
+            int seatNumber = int.Parse(SeatNumberNumberBox.Text);
+
+            if (name == null || size == null || transmissionType == null || fuelType == null) {
+                return false;
+            }
+
+            try
+            {
+                string connectionString = "Server=localhost;Port=3306;Database=car_rental_app;Uid=root;Pwd=;";
+                using MySqlConnection connection = new MySqlConnection(connectionString);
+                await connection.OpenAsync();
+
+                string query = "INSERT INTO Car (Name, Size, TransmissionType, FuelType, PricePerDay, SeatNumber) VALUES (@Name, @Size, @TransmissionType, @FuelType, @PricePerDay, @SeatNumber)";
+                using MySqlCommand command = new MySqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Size", size);
+                command.Parameters.AddWithValue("@TransmissionType", transmissionType);
+                command.Parameters.AddWithValue("@FuelType", fuelType);
+                command.Parameters.AddWithValue("@PricePerDay", pricePerDay);
+                command.Parameters.AddWithValue("@SeatNumber", seatNumber);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine("Car added successfully!");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        private async void AddCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isLoading) return;
+
+            isLoading = true;
+
+            if (await AddCar())
+            {
+                FailTextBlock.Visibility = Visibility.Collapsed;
+                SuccessTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                SuccessTextBlock.Visibility = Visibility.Collapsed;
+                FailTextBlock.Visibility = Visibility.Visible;
+            }
+
+            isLoading = false;
         }
     }
 }
